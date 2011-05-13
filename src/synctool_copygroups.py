3df568a7ddc2ad194f8b226d2bd3ea091cc62032
Walter de Jong <walter@heiho.net>
2011-05-13 22:46:00 +0200

    added (empty) copygroups source file

---
+++ src/synctool_copygroups.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+#
+#	synctool_copygroups.py	WJ111
+#
+#   synctool by Walter de Jong <walter@heiho.net> (c) 2003-2011
+#
+#   synctool COMES WITH NO WARRANTY. synctool IS FREE SOFTWARE.
+#   synctool is distributed under terms described in the GNU General Public
+#   License.
+#
+
+
+
+
+
+# EOB
