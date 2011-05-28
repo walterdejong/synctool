7cca6fb96bb2d7cd1398f31b23f6b203cee8277a
Walter de Jong <walter@heiho.net>
2011-05-28 12:43:35 +0200

    copy_file() does take an object

--- src/synctool.py
+++ src/synctool.py
@@ -474,7 +474,7 @@ def compare_files(obj):
 			need_update = True
 		
 		if need_update:
-			copy_file(src_path, dest_path)
+			copy_file(obj)
 			set_owner(dest_path, src_stat.uid, src_stat.gid)
 			set_permissions(dest_path, src_stat.mode)
 			unix_out('')
@@ -549,8 +549,11 @@ def compare_files(obj):
 	return need_update
 
 
-def copy_file(src, dest):
-	if path_isfile(dest):
+def copy_file(obj):
+	src = obj.src_path
+	dest = obj.dest_path
+	
+	if obj.dest_isFile():
 		unix_out('cp %s %s.saved' % (dest, dest))
 	
 	unix_out('umask 077')
@@ -560,7 +563,7 @@ def copy_file(src, dest):
 		old_umask = os.umask(077)
 		
 		if synctool_param.BACKUP_COPIES:
-			if path_isfile(dest):
+			if obj.dest_isFile():
 				verbose('  saving %s as %s.saved' % (dest, dest))
 				try:
 					shutil.copy2(dest, '%s.saved' % dest)
@@ -571,11 +574,11 @@ def copy_file(src, dest):
 		try:
 			shutil.copy2(src, dest)			# copy file and stats
 		except:
-			stderr('failed to copy %s to %s' % (src, dest))
+			stderr('failed to copy %s to %s' % (obj.print_src(), dest))
 		
 		os.umask(old_umask)
 	else:
-		if path_isfile(dest) and synctool_param.BACKUP_COPIES:
+		if obj.dest_isFile() and synctool_param.BACKUP_COPIES:
 			verbose('  saving %s as %s.saved' % (dest, dest))
 		
 		verbose(dryrun_msg('  cp %s %s' % (src, dest)))
