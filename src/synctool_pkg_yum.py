a01b6b191a2604c116b00eba0b1b7f507749349f
Walter de Jong <walter@heiho.net>
2011-06-12 10:48:36 +0200

    added synctool_pkg module for yum

---
+++ src/synctool_pkg_yum.py
@@ -0,0 +1,68 @@
+#
+#	synctool_pkg_yum.py		WJ111
+#
+#   synctool by Walter de Jong <walter@heiho.net> (c) 2003-2011
+#
+#   synctool COMES WITH NO WARRANTY. synctool IS FREE SOFTWARE.
+#   synctool is distributed under terms described in the GNU General Public
+#   License.
+#
+
+import synctool_lib
+
+from synctool_lib import verbose
+from synctool_pkgclass import SyncPkg
+
+import string
+
+
+class SyncPkgYum(SyncPkg):
+	'''package installer class for yum'''
+	
+	def __init__(self):
+		SyncPkg.__init__(self)
+	
+	
+	def list(self, pkgs = None):
+		SyncPkg.list(self, pkgs)
+		
+		cmd = 'yum list installed'
+		
+		if pkgs:
+			cmd = cmd + ' ' + string.join(pkgs)
+		
+		synctool_lib.DRY_RUN = False
+		synctool_lib.shell_command(cmd)
+		synctool_lib.DRY_RUN = self.dryrun
+	
+	
+	def install(self, pkgs):
+		SyncPkg.install(self, pkgs)
+
+		cmd = 'yum -y install ' + string.join(pkgs)
+		
+		synctool_lib.shell_command(cmd)
+	
+	
+	def remove(self, pkgs):
+		SyncPkg.remove(self, pkgs)
+		
+		cmd = 'yum -y remove ' + string.join(pkgs)
+		
+		synctool_lib.shell_command(cmd)
+	
+	
+	def upgrade(self):
+		SyncPkg.upgrade(self)
+		
+		if self.dryrun:
+			cmd = 'yum -y check-update'
+		else:
+			cmd = 'yum -y update'
+		
+		synctool_lib.DRY_RUN = False
+		synctool_lib.shell_command(cmd)
+		synctool_lib.DRY_RUN = self.dryrun
+
+
+# EOB
