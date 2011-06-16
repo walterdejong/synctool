b64436b9022f937d58d26eca1c8d668d08715d24
Walter de Jong <walter@sara.nl>
2011-06-16 12:40:08 +0200

    shell=True does not take an array as first argument (but simply the command as string)

--- src/synctool_lib.py
+++ src/synctool_lib.py
@@ -415,7 +415,7 @@ def shell_command(cmd):
 		
 		if use_subprocess:
 			try:
-				subprocess.call(cmd_arr, shell=True)
+				subprocess.call(cmd, shell=True)
 			except OSError, reason:
 				stderr("failed to run shell command '%s' : %s" % (prettypath(cmd), reason))
 		else:
