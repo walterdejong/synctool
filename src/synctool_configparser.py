df10d8f4f1fd495a990414e64b4fd071c58eab4a
Walter de Jong <walter@heiho.net>
2013-06-09 16:55:13 +0200

    refuse certain settings of masterdir

--- src/synctool_configparser.py
+++ src/synctool_configparser.py
@@ -232,12 +232,7 @@ def config_masterdir(arr, configfile, lineno):
 	synctool_param.MASTERDIR = synctool_lib.strip_trailing_slash(d)
 	synctool_param.MASTER_LEN = len(synctool_param.MASTERDIR) + 1
 
-	if synctool_param.MASTERDIR == '$masterdir':
-		stderr("%s:%d: masterdir can not be set to '$masterdir', sorry" %
-			(configfile, lineno))
-		return 1
-
-	if not synctool_param.MASTERDIR or synctool_param.MASTERDIR == '/':
+	if synctool_param.MASTERDIR in ('', '/', '$masterdir'):
 		stderr("%s:%d: masterdir can not be set to '%s', sorry" %
 			(configfile, lineno, synctool_param.MASTERDIR))
 		return 1
