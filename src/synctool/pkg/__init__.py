2ddbdbe1e2c843983abb6ac42bc92154f3c6f44a
Walter de Jong <walter@heiho.net>
2013-06-10 17:10:18 +0200

    start of libdir

---
+++ src/synctool/pkg/__init__.py
@@ -0,0 +1,9 @@
+#
+#	synctool	WJ113
+#
+#   synctool by Walter de Jong <walter@heiho.net> (c) 2003-2013
+#
+#   synctool COMES WITH NO WARRANTY. synctool IS FREE SOFTWARE.
+#   synctool is distributed under terms described in the GNU General Public
+#   License.
+#
