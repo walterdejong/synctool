6f22a9fe4d870fd18df862bc9d4b1b78013cfb19
Walter de Jong <walter@heiho.net>
2013-06-29 15:42:12 +0200

    else duplicate output :(

--- src/synctool/overlay.py
+++ src/synctool/overlay.py
@@ -268,16 +268,19 @@ def overlay_pass2(filelist, filedict):
 				entry2.src_stat.is_dir())) and
 				entry.importance == entry2.importance):
 
-				stderr('error: duplicate source paths in repository '
-						'for:\n'
-						'error: %s\n'
-						'error: %s\n' % (entry.print_src(),
-										entry2.print_src())
-
-				synctool.lib.terse(synctool.lib.TERSE_ERROR,
+				if synctool.param.TERSE:
+					synctool.lib.terse(synctool.lib.TERSE_ERROR,
 								'duplicate source paths in repository for:')
-				synctool.lib.terse(synctool.lib.TERSE_ERROR, entry.src_path)
-				synctool.lib.terse(synctool.lib.TERSE_ERROR, entry2.src_path)
+					synctool.lib.terse(synctool.lib.TERSE_ERROR,
+										entry.src_path)
+					synctool.lib.terse(synctool.lib.TERSE_ERROR,
+										entry2.src_path)
+				else:
+					stderr('error: duplicate source paths in repository '
+							'for:\n'
+							'error: %s\n'
+							'error: %s\n' % (entry.print_src(),
+											entry2.print_src())
 				continue
 
 			else:
