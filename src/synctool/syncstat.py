3b2a43e190c37e7e4bd3a5b340fa53fd781480e1
Walter de Jong <walter@heiho.net>
2014-02-17 18:08:50 +0100

    syncstat uses module pwdgrp

--- src/synctool/syncstat.py
+++ src/synctool/syncstat.py
@@ -12,11 +12,10 @@
 
 import os
 import stat
-import pwd
-import grp
 import errno
 
 from synctool.lib import error
+import synctool.pwdgrp
 
 
 class SyncStat(object):
@@ -77,63 +76,69 @@ class SyncStat(object):
             self.size = statbuf.st_size
 
     def is_dir(self):
+        '''Returns True if it's a directory'''
+
         return (self.entry_exists and stat.S_ISDIR(self.mode))
 
     def is_file(self):
+        '''Returns True if it's a regular file'''
+
         return (self.entry_exists and stat.S_ISREG(self.mode))
 
     def is_link(self):
+        '''Returns True if it's a symbolic link'''
+
         return (self.entry_exists and stat.S_ISLNK(self.mode))
 
     def is_fifo(self):
+        '''Returns True if it's a FIFO'''
+
         return (self.entry_exists and stat.S_ISFIFO(self.mode))
 
     def is_sock(self):
+        '''Returns True if it's a socket file'''
+
         return (self.entry_exists and stat.S_ISSOCK(self.mode))
 
     def is_chardev(self):
+        '''Returns True if it's a character device file'''
+
         return (self.entry_exists and stat.S_ISCHR(self.mode))
 
     def is_blockdev(self):
+        '''Returns True if it's a block device file'''
+
         return (self.entry_exists and stat.S_ISCHR(self.mode))
 
     def filetype(self):
+        '''Returns the file type part of the mode'''
+
         return stat.S_IFMT(self.mode)
 
     def exists(self):
+        '''Returns True if it exists'''
+
         return self.entry_exists
 
     def is_exec(self):
+        '''Returns True if its mode has any 'x' bit set'''
+
         return (self.entry_exists and ((self.mode & 0111) != 0))
 
     def ascii_uid(self):
-        '''get the name for this uid'''
+        '''Returns the username for this uid'''
 
         if not self.entry_exists:
-            return None
+            raise ValueError()
 
-        try:
-            entry = pwd.getpwuid(self.uid)
-            return entry[0]
-
-        except KeyError:
-            pass
-
-        return '%d' % self.uid
+        return synctool.pwdgrp.pw_name(self.uid)
 
     def ascii_gid(self):
-        '''get the name for this gid'''
+        '''Returns the group for this gid'''
 
         if not self.entry_exists:
-            return None
+            raise ValueError()
 
-        try:
-            entry = grp.getgrgid(self.gid)
-            return entry[0]
-
-        except KeyError:
-            pass
-
-        return '%d' % self.gid
+        return synctool.pwdgrp.grp_name(self.gid)
 
 # EOB
