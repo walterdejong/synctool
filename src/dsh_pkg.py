31be63c6b75834d88fd90cdfc345596ae078e63d
Walter de Jong <walter@heiho.net>
2019-08-11 11:06:58 +0200

    use python2 as interpreter

--- src/dsh_pkg.py
+++ src/dsh_pkg.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 #
 #   dsh_pkg.py  WJ114
 #
--- src/synctool_client_pkg.py
+++ src/synctool_client_pkg.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2
 #
 #   synctool_client_pkg.py  WJ114
 #
